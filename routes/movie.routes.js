9f4701c293e4fe9c4857da9419f886ba0da3bfad
bilalAhmad69 <bilal.khattak69@gmail.com>
2022-02-28 13:03:51 +0500

    protecting routes and generating jwt token

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,12 +1,15 @@
 const { getMovies, postMovie, getSpecificMovie, deleteMovie, getMoviesByGenre, totalBusinessByActor, updateMovie } = require("../controller/movie.controller");
+const auth = require("../middleware/auth");
+const admin = require  ("../middleware/admin")
 const express = require("express");
 const router = express.Router();
 router.get("/" , getMovies);
 router.get("/byGenre", getMoviesByGenre);
 router.get("/totalbusiness/:id" , totalBusinessByActor);
 router.get("/:id", getSpecificMovie);
-router.post("/" , postMovie);
-router.put("/:id" , updateMovie);
+// post and put routes are protected only admin can access....
+router.post("/" , [auth , admin], postMovie);
+router.put("/:id" ,[auth , admin], updateMovie);
 router.delete("/:id" , deleteMovie);
 
 
