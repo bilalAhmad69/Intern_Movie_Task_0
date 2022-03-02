d533120915e3e7ee111cb1a128ef286276b5461b
bilalAhmad69 <bilal.khattak69@gmail.com>
2022-03-02 12:23:31 +0500

    front end validation for reviews using joi

--- controller/review.controller.js
+++ controller/review.controller.js
@@ -1,4 +1,4 @@
-const {MovieReview} = require ("../models/review.models");
+const {MovieReview, validateReview} = require ("../models/review.models");
 const {User} = require ("../models/user.models");
 const {Movie} = require ("../models/movie.models");
 
@@ -23,12 +23,14 @@ const getMovieReview = async (req , res) =>{
 // >>>>>>>>>>>> Post Review Start <<<<<<<<<<<<<<<< //
 
 const postMovieReview = async (req, res) =>{
+    const {error} = validateReview(req.body);
+    if (error) return res.status(400).send(error.message);
     const {userId , movieId , review} = req.body;
     try{
     const user = await User.findById(userId);
     if(!user) return res.status(400).send("Cannot give Rating  You are not Registered");
     const movie = await Movie.findById(movieId);
-    if(!movie) return res.status(400).send("Bad Request ");
+    if(!movie) return res.status(404).send("Movie not Found ");
     const movieReview = new MovieReview({
         user : userId,
         movie : movieId,
@@ -41,7 +43,7 @@ const postMovieReview = async (req, res) =>{
 }
     catch (e)
     {
-        res.send(e.message);
+        res.send(e.message);           
     }
 }
 
--- models/review.models.js
+++ models/review.models.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Joi = require ("joi");
 const movieReviewSchema = mongoose.Schema ({
      user :{
          type : mongoose.Types.ObjectId,
@@ -16,5 +17,17 @@ const movieReviewSchema = mongoose.Schema ({
 }) ;
 
 const MovieReview = mongoose.model("Review" , movieReviewSchema);
+// front end validation using joi
+function validateReview (review) {
 
+    const schema = Joi.object({
+        userId : Joi.string().hex().length(24).required(),
+        movieId : Joi.string().hex().length(24).required(),
+        review : Joi.string().required()
+
+    });
+    return schema.validate(review);
+
+}
 exports.MovieReview = MovieReview;
+exports.validateReview = validateReview;
