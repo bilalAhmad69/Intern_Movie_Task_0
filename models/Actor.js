e86861d324cdd15442c0b3453a9dcd3bd5c86216
bilalAhmad69 <bilal.khattak69@gmail.com>
2022-03-06 03:59:02 +0500

    Re write the folder name removing extra comment etc

---
+++ models/Actor.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Joi = require ("joi");
+const actorSchema = mongoose.Schema({
+     name : {
+         type: String,
+         minLength: 3,
+         maxLength : 50,
+         required : true,
+     },
+     age :{
+         type : Number ,
+         min : (3), 
+         max : 150,
+         required : true,
+     },
+     gender : {
+         type : String,
+         enum : ['Male' , 'Female' , 'Transgender'],
+         default : 'Male'
+     }
+});
+
+const Actor = new mongoose.model('Actor'  , actorSchema);
+exports.Actor = Actor;
