7bf6558f6610c5c48685e3ce5d0a23fef00a101b
bilalAhmad69 <bilal.khattak69@gmail.com>
2022-03-07 11:48:41 +0500

    Email Verification update user Password done

---
+++ services/mailSender.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+    service:"gmail",
+    auth: {
+        user : process.env.MYEMAIL,
+        pass : process.env.PASSWORD
+    }
+});
+
+module.exports =  mailSender  = async (recieverEmail , subject , text ) =>{
+const mailOption = {
+    from : process.env.MYEMAIL, 
+    to : recieverEmail,
+    subject : subject,
+    text : text
+};
+try{
+await transporter.sendMail(mailOption);
+return "Message Sent";
+}
+catch (e)
+{
+    return e.message;
+}
+}
+
